44351f04dc0ecdc650d682b0f2e2c7bbc90aaa2a
Muhammad Asif Khan <asifkhan6495607@gmail.com>
2025-09-21 21:49:11 +0600

    Calculate GCD & LCM
    User can enter 2 number & calculate GCD & LCM between 2 number.

---
+++ gcd_lcm.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, gcd, i, lcm;
+    printf("Enter the first number: ");
+    scanf("%d", &n1);
+    printf("Enter the second number: ");
+    scanf("%d", &n2);
+
+    for (i = 1; i <= n1 && i <= n2; i++)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    lcm = (n1 * n2) / gcd;
+
+    printf("\nHere GCD of %d & %d : %d", n1, n2, gcd);
+    printf("\nHere LCM of %d & %d : %d", n1, n2, lcm);
+    return 0;
+}
